af2dc523670c705ff19b8b1f9c86830ade6f61d6
LorranSutter <sutterlorran@gmail.com>
2026-05-18 21:38:24 -0400

    WIP 2025 quest 15 part 3

--- 2025/event/quest15/main.py
+++ 2025/event/quest15/main.py
@@ -1,7 +1,11 @@
 import os
+import matplotlib.pyplot as plt
 from typing import List, Tuple
 from collections import deque
 from dataclasses import dataclass
+from shapely.geometry import LineString, Point
+from shapely.plotting import plot_line, plot_points
+from itertools import combinations
 
 from utils.timer import timer
 from utils.utils import tcolors
@@ -29,13 +33,20 @@ class Coordinate:
     def __str__(self) -> str:
         return f"({self.x}, {self.y})"
 
+    def __add__(self, other):
+        return Coordinate(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Coordinate(self.x - other.x, self.y - other.y)
+
 
 @timer
 def part1():
     instructions = parse_file("input01.txt")
-    print(f"Instructions: {instructions}")
 
-    corners, boundaries = find_corners_and_boundaries(instructions, (0, 0))
+    corners, boundaries = find_corners_and_boundaries(
+        instructions, Coordinate(0, 0), Coordinate(-1, 0)
+    )
     tunnel, corners = build_tunnel(corners, boundaries)
     path_length, path = find_shortest_path_with_trace(tunnel, corners[0], corners[-1])
 
@@ -59,9 +70,10 @@ def part1():
 @timer
 def part2():
     instructions = parse_file("input02.txt")
-    print(f"Instructions: {instructions}")
 
-    corners, boundaries = find_corners_and_boundaries(instructions, (0, 0))
+    corners, boundaries = find_corners_and_boundaries(
+        instructions, Coordinate(0, 0), Coordinate(-1, 0)
+    )
     tunnel, corners = build_tunnel(corners, boundaries)
     path_length, path = find_shortest_path_with_trace(tunnel, corners[0], corners[-1])
 
@@ -76,34 +88,102 @@ def part2():
 @timer
 def part3():
     # TODO: Implement part 3
-    lines = parse_file("input_sample03.txt")
-    pass
+    instructions = parse_file("input_sample03.txt")
+
+    corners, expanded_corners = find_expanded_corners(
+        instructions, Coordinate(0, 0), Coordinate(-1, 0)
+    )
+
+    # Build the walls of the tunnel
+    walls = build_walls(corners)
+
+    # Build all possible edges that connect the expanded corners
+    expanded_corner_combs = list(combinations(expanded_corners, 2))
+    all_edges = []
+    for pair in expanded_corner_combs:
+        all_edges.append(LineString([(pair[0].x, pair[0].y), (pair[1].x, pair[1].y)]))
+
+    # Find the edges that do not intersect with any walls
+    edges = tuple(edge for edge in all_edges if not any(edge.intersects(walls)))
+
+    _, ax = plt.subplots()
+
+    # Built-in plotting functions
+    for wall in walls:
+        plot_line(wall, ax=ax, color="blue")
+
+    for edge in edges:
+        plot_line(edge, ax=ax, color="red")
+
+    expanded_corners = [Point(c.x, c.y) for c in expanded_corners]
+    plot_points(expanded_corners, ax=ax, color="red")
+
+    plt.show()
 
 
-def find_corners_and_boundaries(
-    instructions: List[str], start: Tuple[int, int]
-) -> Tuple[List[Coordinate], Tuple[Coordinate, Coordinate]]:
-    pos = Coordinate(start[0], start[1])
-    dir = Coordinate(-1, 0)
-    boundaries = (Coordinate(0, 0), Coordinate(0, 0))
-    corners = [pos]
+def find_expanded_corners(
+    instructions: List[str], start: Coordinate, start_dir: Coordinate
+) -> Tuple[List[Coordinate], List[Coordinate]]:
+    expanded_corner_map = {
+        (Coordinate(-1, 0), Coordinate(0, 1)): Coordinate(-1, -1),
+        (Coordinate(-1, 0), Coordinate(0, -1)): Coordinate(-1, 1),
+        (Coordinate(1, 0), Coordinate(0, 1)): Coordinate(1, -1),
+        (Coordinate(1, 0), Coordinate(0, -1)): Coordinate(1, 1),
+        (Coordinate(0, -1), Coordinate(1, 0)): Coordinate(-1, -1),
+        (Coordinate(0, 1), Coordinate(1, 0)): Coordinate(-1, 1),
+        (Coordinate(0, -1), Coordinate(-1, 0)): Coordinate(1, -1),
+        (Coordinate(0, 1), Coordinate(-1, 0)): Coordinate(1, 1),
+    }
+    pos = start
+    dir = start_dir
+    corners = []
+    expanded_corners = []
     for instruction in instructions:
         side, length = instruction[0], int(instruction[1:])
 
         if side == "R":
-            pos = Coordinate(pos.x + dir.y * length, pos.y - dir.x * length)
-            dir = Coordinate(dir.y, -dir.x)
+            new_pos = Coordinate(pos.x + dir.y * length, pos.y - dir.x * length)
+            new_dir = Coordinate(dir.y, -dir.x)
         else:
-            pos = Coordinate(pos.x - dir.y * length, pos.y + dir.x * length)
-            dir = Coordinate(-dir.y, dir.x)
+            new_pos = Coordinate(pos.x - dir.y * length, pos.y + dir.x * length)
+            new_dir = Coordinate(-dir.y, dir.x)
 
+        expanded_corner_dir = expanded_corner_map[(dir, new_dir)]
+        expanded_corners.append(pos + expanded_corner_dir)
         corners.append(pos)
-        boundaries = (
-            Coordinate(min(boundaries[0].x, pos.x), min(boundaries[0].y, pos.y)),
-            Coordinate(max(boundaries[1].x, pos.x), max(boundaries[1].y, pos.y)),
+
+        dir = new_dir
+        pos = new_pos
+
+    expanded_corners[0] = corners[0]
+    expanded_corners[-1] = corners[-1]
+
+    return corners, expanded_corners
+
+
+def build_walls(corners: List[Coordinate]) -> List[LineString]:
+    walls = []
+    for i in range(1, len(corners) - 2):
+        walls.append(
+            LineString(
+                [(corners[i].x, corners[i].y), (corners[i + 1].x, corners[i + 1].y)]
+            )
         )
 
-    return corners, boundaries
+    # We remove the starting and ending walls to avoid detecting it as intersections
+    
+    # Remove the starting point from the first wall
+    dir = get_direction(corners[0], corners[1])
+    first_wall = LineString([(corners[0].x + dir.x, corners[0].y + dir.y), (corners[1].x, corners[1].y)])
+
+    # Remove the ending point from the last wall
+    dir = get_direction(corners[-2], corners[-1])
+    last_wall = LineString([(corners[-2].x, corners[-2].y), (corners[-1].x - dir.x, corners[-1].y - dir.y)])
+
+    walls.insert(0, first_wall)
+    walls.append(last_wall)
+
+    return walls
 
 
 def build_tunnel(
@@ -185,6 +265,32 @@ def find_shortest_path_with_trace(
     return -1, []  # No path found
 
 
+def find_corners_and_boundaries(
+    instructions: List[str], start: Coordinate, start_dir: Coordinate
+) -> Tuple[List[Coordinate], Tuple[Coordinate, Coordinate]]:
+    pos = start
+    dir = start_dir
+    boundaries = (Coordinate(0, 0), Coordinate(0, 0))
+    corners = [pos]
+    for instruction in instructions:
+        side, length = instruction[0], int(instruction[1:])
+
+        if side == "R":
+            pos = Coordinate(pos.x + dir.y * length, pos.y - dir.x * length)
+            dir = Coordinate(dir.y, -dir.x)
+        else:
+            pos = Coordinate(pos.x - dir.y * length, pos.y + dir.x * length)
+            dir = Coordinate(-dir.y, dir.x)
+
+        corners.append(pos)
+        boundaries = (
+            Coordinate(min(boundaries[0].x, pos.x), min(boundaries[0].y, pos.y)),
+            Coordinate(max(boundaries[1].x, pos.x), max(boundaries[1].y, pos.y)),
+        )
+
+    return corners, boundaries
+
+
 def print_tunnel(tunnel: List[List[str]]) -> None:
     for row in tunnel:
         print("".join(row))
@@ -207,6 +313,6 @@ def parse_file(file_name: str) -> Tuple[str]:
     return instructions
 
 
-part1()
-part2()
-# part3()
+# part1()
+# part2()
+part3()
